aa40e80299a24c23822c00eef0de9706d4f67876
Damien Zammit <damien@zamaudio.com>
2015-09-15 14:44:38 +1000

    Added stubs for service requests
    Signed-off-by: Damien Zammit <damien@zamaudio.com>

--- firmware/interrupts.c
+++ firmware/interrupts.c
@@ -16,73 +16,196 @@
 #include "smu.h"
 #include "interrupts.h"
 
+static void halt()
+{
+}
+
+static void set_phyln(int onoff)
+{
+}
+
+static void set_ddiphy(int onoff)
+{
+}
+
+static void set_cascadepll(int onoff)
+{
+}
+
+static void set_x16off(void)
+{
+}
+
+static void config_lclkdpm(void)
+{
+}
+
+static void flush_datacache(void)
+{
+}
+
+static void flush_instcache(void)
+{
+}
+
+static void config_vpc(void)
+{
+}
+
+static void config_bapm(void)
+{
+}
+
+static void config_tdc(void)
+{
+}
+
+static void config_lpmx(void)
+{
+}
+
+static void config_htc(void)
+{
+}
+
+static void config_thermal(void)
+{
+}
+
+static void config_voltage(void)
+{
+}
+
+static void config_tdp(void)
+{
+}
+
+static void set_pm(int onoff)
+{
+}
+
+static void config_nbdpm()
+{
+}
+
+static void config_loadline()
+{
+}
+
+static void adjust_loadline()
+{
+}
+
+static void reconfigure()
+{
+}
+
+static void pciepllswitch()
+{
+}
+
+static void set_bapm(int onoff)
+{
+}
+
 void SMUServiceRequest(unsigned int level, void *ctx)
 {
 	int requestid;
 	REG_WRITE(0xe0003004, 1);
 	requestid = REG_READ(0xe0003000);
 	requestid &= 0x1fffe;
-	
+	requestid >>= 1;
+
 	switch(requestid) {
 	case SMC_MSG_HALT:
+		halt();
 		break;
 	case SMC_MSG_PHY_LN_OFF:
+		set_phyln(0);
 		break;
 	case SMC_MSG_PHY_LN_ON:
+		set_phyln(1);
 		break;
 	case SMC_MSG_DDI_PHY_OFF:
+		set_ddiphy(0);
 		break;
 	case SMC_MSG_DDI_PHY_ON:
+		set_ddiphy(1);
 		break;
 	case SMC_MSG_CASCADE_PLL_OFF:
+		set_cascadepll(0);
 		break;
 	case SMC_MSG_CASCADE_PLL_ON:
+		set_cascadepll(1);
 		break;
 	case SMC_MSG_PWR_OFF_x16:
+		set_x16off();
 		break;
 	case SMC_MSG_CONFIG_LCLK_DPM:
+		config_lclkdpm();
 		break;
 	case SMC_MSG_FLUSH_DATA_CACHE:
+		flush_datacache();
 		break;
 	case SMC_MSG_FLUSH_INSTRUCTION_CACHE:
+		flush_instcache();
 		break;
 	case SMC_MSG_CONFIG_VPC_ACCUMULATOR:
+		config_vpc();
 		break;
 	case SMC_MSG_CONFIG_BAPM:
+		config_bapm();
 		break;
 	case SMC_MSG_CONFIG_TDC_LIMIT:
+		config_tdc();
 		break;
 	case SMC_MSG_CONFIG_LPMx:
+		config_lpmx();
 		break;
 	case SMC_MSG_CONFIG_HTC_LIMIT:
+		config_htc();
 		break;
 	case SMC_MSG_CONFIG_THERMAL_CNTL:
+		config_thermal();
 		break;
 	case SMC_MSG_CONFIG_VOLTAGE_CNTL:
+		config_voltage();
 		break;
 	case SMC_MSG_CONFIG_TDP_CNTL:
+		config_tdp();
 		break;
 	case SMC_MSG_EN_PM_CNTL:
+		set_pm(1);
 		break;
 	case SMC_MSG_DIS_PM_CNTL:
+		set_pm(0);
 		break;
 	case SMC_MSG_CONFIG_NBDPM:
+		config_nbdpm();
 		break;
 	case SMC_MSG_CONFIG_LOADLINE:
+		config_loadline();
 		break;
 	case SMC_MSG_ADJUST_LOADLINE:
+		adjust_loadline();
 		break;
 	case SMC_MSG_RECONFIGURE:
+		reconfigure();
 		break;
 	case SMC_MSG_PCIE_PLLSWITCH:
+		pciepllswitch();
 		break;
 	case SMC_MSG_ENABLE_BAPM:
+		set_bapm(1);
 		break;
 	case SMC_MSG_DISABLE_BAPM:
+		set_bapm(0);
 		break;
 	default:
 		break;
 	}
+
+	REG_WRITE(0xe0003004, 3);
 }
 
 void MicoISRHandler(void)
